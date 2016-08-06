c6121eb98fe3630a23470156b42c9de98cae321c
Mark Sergienko <ffab00@gmail.com>
2016-08-05 22:46:43 -0400

    Update linkStrategy.js
    Updated link=>LINK to be compatible with react-rte

--- draft-js-toolbar-plugin/src/linkStrategy.js
+++ draft-js-toolbar-plugin/src/linkStrategy.js
@@ -4,7 +4,7 @@ function findLinkEntities(contentBlock, callback) {
   contentBlock.findEntityRanges(
     (character) => {
       const entityKey = character.getEntity();
-      return entityKey !== null && Entity.get(entityKey).getType() === 'link';
+      return entityKey !== null && Entity.get(entityKey).getType() === 'LINK';
     }, callback
   );
 }
